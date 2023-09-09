ec0cbc2d5496201889964ca0e9b02244fbfab3a7
Eky Galih <ekkygalih8@gmail.com>
2023-09-09 21:44:34 +0800

    user middleware

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,5 +1,13 @@
 import LayoutUser from "../layouts/user/LayoutUser";
 
+export const metadata = {
+    title: 'User | Dashboard',
+    openGraph: {
+      title: 'Dashboard',
+      description: 'App surat bpkad'
+    }
+  }
+
 export default function DashboardUser() {
     return (
         <>
